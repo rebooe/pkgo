3ab5d3fca3ed4f73457baced5ef542b108f87888
air <1023024859@qq.com>
2023-09-20 20:15:27 +0800

    1. 增加发送邮件模块

---
+++ email/email.go
@@ -0,0 +1,34 @@
+package email
+
+import (
+	"fmt"
+	"net/smtp"
+	"strings"
+
+	"github.com/rebooe/pkg-go/util"
+)
+
+type Email struct {
+	smtpHost string    // 邮件服务地址
+	from     string    // 发送放账户
+	fromName string    // 发送方名称
+	auth     smtp.Auth // 邮件授权信息
+}
+
+func NewEmail(smtpHost string, from, pwd, fromName string) *Email {
+	host, _, _ := strings.Cut(smtpHost, ":")
+	auth := smtp.PlainAuth("", from, pwd, host)
+
+	return &Email{
+		smtpHost: smtpHost,
+		from:     from,
+		fromName: fromName,
+		auth:     auth,
+	}
+}
+
+func (em *Email) SendMail(to []string, subject string, body string) error {
+	message := fmt.Sprintf("From: %s <%s>\r\nSubject: %s\r\n\r\n%s",
+		em.fromName, em.from, subject, body)
+	return smtp.SendMail(em.smtpHost, em.auth, em.from, to, util.StringToBytes(message))
+}
